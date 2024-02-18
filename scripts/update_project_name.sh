1f790ccc77fd3f137039475c87b05821a9cdd66d
mthnguyener <>
2024-02-18 17:48:04 -0500

    fixing missing $

--- scripts/update_project_name.sh
+++ scripts/update_project_name.sh
@@ -13,14 +13,14 @@ NEW_PROJECT="$2"
 echo "Current working directory: $(pwd)"
 
 # Copy the template project to a new directory
-cp -r "../OLD_PROJECT" "../NEW_PROJECT"
+cp -r "../$OLD_PROJECT" "../$NEW_PROJECT"
 
 # Print the current working directory where new project is located
-echo "Project built in: $(pwd)/NEW_PROJECT"
+echo "Project built in: $(pwd)/$NEW_PROJECT"
 
 # Update file contents
-cd ../NEW_PROJECT
-rm -rf OLD_PROJECT.egg-info
+cd ../$NEW_PROJECT
+rm -rf $OLD_PROJECT.egg-info
 rm -rf %%
 rm -rf cache
 rm -rf docker/secrets
@@ -31,10 +31,10 @@ rm -rf .idea
 rm -rf venv
 rm -rf wheels
 rm usr_vars
-mv OLD_PROJECT NEW_PROJECT
-find . -type f -exec sed -i "s/OLD_PROJECT/NEW_PROJECT/g" {} +
+mv $OLD_PROJECT $NEW_PROJECT
+find . -type f -exec sed -i "s/$OLD_PROJECT/$NEW_PROJECT/g" {} +
 
 # Removing git directory
 rm -rf .git
 
-echo "New project 'NEW_PROJECT' created successfully!"
+echo "New project '$NEW_PROJECT' created successfully!"
